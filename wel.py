2451e7b343bac9fdefa827db9e6d3f6bac13f492
minhaj02 <mdminhajulislam2016@gmail.com>
2022-05-17 20:34:25 +0600

    remove unnecessary text

--- wel.py
+++ wel.py
@@ -1,7 +1,6 @@
 print("Welcome to git!")
-<<<<<<< Updated upstream
+
+
+print("Git is a very important tool for a programmer.")
 
 print("So I want to git.")
-=======
-print("Git is a very important tool for a programmer.")
->>>>>>> Stashed changes
